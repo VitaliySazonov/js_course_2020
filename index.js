97fd4380f2d20c042d2785ccf6e61a10946801c7
Vito <topvetal@gmail.com>
2020-04-04 01:46:44 +0300

    proto and this

--- index.js
+++ index.js
@@ -1,7 +1,71 @@
+// const person = {
+//     name: 'Maxim',
+//     age: 25,
+//     greet: function () {
+//         console.log('Greet!')
+//     }
+// };
+
+// const person = new Object({
+//     name: 'Maxim',
+//     age: 25,
+//     greet: function () {
+//         console.log('Greet!')
+//     }
+// });
+//
+// Object.prototype.sayHello = function () { // задали новуую функцию.
+//     console.log('Hello!')
+// };
+//
+// const lena = Object.create(person);
+// lena.name = 'Elena';
+//
+// // const str = new String('I am string');
+// const str = "I am string";
+
+function hello () {
+    console.log('Hello', this);
+}
+
 const person = {
-    name: 'Maxim',
+    name: 'Vito',
     age: 25,
-    greet: function () {
-        console.log('Greet!')
+    sayHello: hello,
+    sayHelloWindow: hello.bind(document),
+    logInfo: function (job, phone) {
+        console.group(`${this.name} info:`);
+        console.log(`Name is ${this.name}`);
+        console.log(`Age is ${this.age}`);
+        console.log(`Job is ${job}`);
+        console.log(`Phone is ${phone}`);
+        console.groupEnd();
     }
 };
+
+
+const lena = {
+    name: 'Elena',
+    age: 23
+};
+// person.logInfo.bind(lena, 'front-end', '8-9998')(); // делает функцию которую можна вызвать где угодно
+// person.logInfo.call(lena, 'front-end', '8-9998'); // сразу вызывает функцию
+person.logInfo.apply(lena, ['front-end', '8-9998']); // отличаются call | apply только способом передачи данных в функцию
+// =================
+
+const array = [1, 2, 3, 4, 5];
+//
+// function multBy(arr, n) {
+//     return arr.map(function (i) {
+//         return i * n;
+//     });
+// }
+Array.prototype.multBy = function(n) {
+    console.log('multBy ', this);
+    return this.map(function (i) {
+        return i * n;
+    });
+};
+console.log(array.multBy(20));
+
+
