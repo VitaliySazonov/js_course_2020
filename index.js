28748f164b172c394741388f1db35abff93ab40b
Vito <topvetal@gmail.com>
2020-04-04 17:18:51 +0300

    proxy

--- index.js
+++ index.js
@@ -344,13 +344,13 @@
 // Async await
 
 
-const delay = ms => {
-    return new Promise((r => setTimeout(() => r(), ms)));
-};
+// const delay = ms => {
+//     return new Promise((r => setTimeout(() => r(), ms)));
+// };
 
 // delay(2000).then(() => console.log('2 Seconds'));
 
-const url = 'https://jsonplaceholder.typicode.com/todos';
+// const url = 'https://jsonplaceholder.typicode.com/todos';
 
 // function fetchTodos() {
 //     console.log('Started...');
@@ -364,32 +364,93 @@ const url = 'https://jsonplaceholder.typicode.com/todos';
 //     .catch(e => console.log(e));
 //
 
-async function fetchAsyncTodos() {
-    console.log('Start...');
-    try {
-        await delay(2000);
-        const response = await fetch(url);
-        const data = await response.json();
-        console.log('Data: ', data);
-    } catch (e) {
-        console.error(e);
-    } finally {
-        console.log('Yes')
-    }
+// async function fetchAsyncTodos() {
+//     console.log('Start...');
+//     try {
+//         await delay(2000);
+//         const response = await fetch(url);
+//         const data = await response.json();
+//         console.log('Data: ', data);
+//     } catch (e) {
+//         console.error(e);
+//     } finally {
+//         console.log('Yes')
+//     }
+//
+// }
+//
+//
+// fetchAsyncTodos().then(() => console.log('End'));
+//
 
+// Proxy
+
+
+const person = {
+    name: 'Vito',
+    age: 25,
+    job: 'Front-end',
+};
+
+const op = new Proxy(person, {
+    get(target, prop) {
+        // console.log(`Get prop ${prop}`);
+        if (!(prop in target)) { // Класный пример op.age_name_job // 25 Vito Frontend
+            return prop
+                .split('_')
+                .map(p => target[p])
+                .join(' ');
+        }
+        return target[prop];
+    },
+    set(target, prop, value) {
+        if(prop in target) {
+            target[prop] = value;
+        } else {
+            throw new Error(`No ${prop} field in target`);
+        }
+    },
+    has(target, prop) { // Должен вернуть либо true либо false
+        return ['age', 'job'].includes(prop)
+    },
+    deleteProperty(target, prop) { // Удаление поля в объекте Proxy
+        console.log('Deleting...', prop);
+        delete target[prop];
+        return true;
+    }
+});
+
+// Proxy functions
+
+const log = text => `Log: ${text}`;
+const fp = new Proxy(log, {
+    apply(target, thisArg, args) {
+        console.log('Calling fn...');
+        return target.apply(thisArg, args).toUpperCase();
+
+    }
+});
+
+// Classes
+
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
 }
 
+const PersonProxy = new Proxy(Person, {
+    construct(target, args) {
+        console.log('Construct...');
+        return new Proxy(new target(...args), {
+            get(t, prop) {
+                console.log(`Getting prop ${prop}`);
+                return t[prop];
+            }
+        });
+    }
+});
 
-fetchAsyncTodos().then(() => console.log('End'));
-
-
-
-
-
-
-
-
-
-
-
+const p = new PersonProxy('Maxim', 30);
 
