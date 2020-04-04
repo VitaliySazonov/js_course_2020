78d5d8b840201d3c99da05c74db764fe1c2d05c1
Vito <topvetal@gmail.com>
2020-04-04 15:32:05 +0300

    class

--- index.js
+++ index.js
@@ -200,39 +200,164 @@
 
 //Object.create
 
-const person = Object.create(
-    {
-        calculateAge() {
-            console.log('Age: ', new Date().getFullYear() - this.birthyear)
-        }
-    },
-    {
-        name: {
-            value: 'Vito',
-            enumerable: true, // false по-умолчанию. Для того чтоб его было видно в цикле for
-            writable: true, // false по-умолчанию. Разрешает писать/изменять данные
-            configurable: true, //false по-умолчанию. Манипулировать с ключами объекта
-        },
-        birthyear: {
-            value: 1993,
-            writable: false,
-            enumerable: true,
-            configurable: false
-        },
-        age: {
-            get() {
-                return new Date().getFullYear() - this.birthyear;
-            },
-            set(value) {
-                document.body.style.background = 'red';
-                console.log('Set age', value);
-            }
-        }
-    });
+// const person = Object.create(
+//     {
+//         calculateAge() {
+//             console.log('Age: ', new Date().getFullYear() - this.birthyear)
+//         }
+//     },
+//     {
+//         name: {
+//             value: 'Vito',
+//             enumerable: true, // false по-умолчанию. Для того чтоб его было видно в цикле for
+//             writable: true, // false по-умолчанию. Разрешает писать/изменять данные
+//             configurable: true, //false по-умолчанию. Манипулировать с ключами объекта
+//         },
+//         birthyear: {
+//             value: 1993,
+//             writable: false,
+//             enumerable: true,
+//             configurable: false
+//         },
+//         age: {
+//             get() {
+//                 return new Date().getFullYear() - this.birthyear;
+//             },
+//             set(value) {
+//                 document.body.style.background = 'red';
+//                 console.log('Set age', value);
+//             }
+//         }
+//     });
 
 // person.name = 'Maxim';
-// hasOwnProperty(key) всегда использовать с for in циклами 
-for (let key in person) {
-    if (person.hasOwnProperty(key)) console.log('Key', key, person[key]);
+// hasOwnProperty(key) всегда использовать с for in циклами
+// for (let key in person) {
+//     if (person.hasOwnProperty(key)) console.log('Key', key, person[key]);
+//
+// }
 
+// ES 6 Class
+
+
+// const animal = {
+//     name: 'Dog',
+//     age: 5,
+//     hasTail: true,
+// };
+
+
+// class Animal {
+//
+//     static type = 'ANIMAL';
+//
+//     constructor(options) {
+//         this.name = options.name;
+//         this.age = options.age;
+//         this.hasTail = options.hasTail;
+//     }
+//
+//     voice() {
+//         console.log('I am animal');
+//     }
+// }
+
+
+// const animal = new Animal({
+//     name: 'Dog',
+//     age: 5,
+//     hasTail: true,
+// });
+
+// class Cat extends Animal {
+//     static type = 'CAT';
+//     constructor(options) {
+//         super(options);
+//         this.color = options.color;
+//     }
+//     voice() {
+//         super.voice();
+//         console.log('I am cat');
+//     }
+//     get ageInfo() {
+//         return this.age * 7;
+//     }
+//     set ageInfo(newAge) {
+//         this.age = newAge;
+//     }
+// }
+//
+// const cat = new Cat({
+//     name: 'Cat',
+//     age: 7,
+//     hasTail: true,
+//     color: 'black'
+// });
+
+
+
+class Component {
+    constructor(selector) {
+        // this.$el = $(selector);
+        this.$el = document.querySelector(selector);
+    }
+    hide() {
+        this.$el.style.display = 'none'
+    }
+    show() {
+        this.$el.style.display = 'block';
+    }
 }
+class Box extends Component {
+    constructor(options) {
+        super(options.selector);
+        this.$el.style.width = this.$el.style.height = options.size + 'px';
+        this.$el.style.background = options.background;
+    }
+}
+
+const box1 = new Box({
+    selector: '#box1',
+    size: 100,
+    background: 'red'
+});
+const box2 = new Box({
+    selector: '#box2',
+    size: 120,
+    background: 'blue'
+});
+
+class Circle extends Box {
+    constructor(options) {
+        super(options);
+        this.$el.style.borderRadius = '50%'
+    }
+}
+
+const circle = new Circle({
+    selector: '#circle',
+    size: 90,
+    background: 'green'
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
