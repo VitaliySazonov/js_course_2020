3f894227541f423ce24f3b6e047231bf82edcce3
Vito <topvetal@gmail.com>
2020-04-04 17:54:14 +0300

    proxy_2

--- index.js
+++ index.js
@@ -386,71 +386,141 @@
 // Proxy
 
 
-const person = {
-    name: 'Vito',
-    age: 25,
-    job: 'Front-end',
-};
-
-const op = new Proxy(person, {
-    get(target, prop) {
-        // console.log(`Get prop ${prop}`);
-        if (!(prop in target)) { // Класный пример op.age_name_job // 25 Vito Frontend
-            return prop
-                .split('_')
-                .map(p => target[p])
-                .join(' ');
-        }
-        return target[prop];
-    },
-    set(target, prop, value) {
-        if(prop in target) {
-            target[prop] = value;
-        } else {
-            throw new Error(`No ${prop} field in target`);
-        }
-    },
-    has(target, prop) { // Должен вернуть либо true либо false
-        return ['age', 'job'].includes(prop)
-    },
-    deleteProperty(target, prop) { // Удаление поля в объекте Proxy
-        console.log('Deleting...', prop);
-        delete target[prop];
-        return true;
-    }
-});
+// const person = {
+//     name: 'Vito',
+//     age: 25,
+//     job: 'Front-end',
+// };
+//
+// const op = new Proxy(person, {
+//     get(target, prop) {
+//         // console.log(`Get prop ${prop}`);
+//         if (!(prop in target)) { // Класный пример op.age_name_job // 25 Vito Frontend
+//             return prop
+//                 .split('_')
+//                 .map(p => target[p])
+//                 .join(' ');
+//         }
+//         return target[prop];
+//     },
+//     set(target, prop, value) {
+//         if(prop in target) {
+//             target[prop] = value;
+//         } else {
+//             throw new Error(`No ${prop} field in target`);
+//         }
+//     },
+//     has(target, prop) { // Должен вернуть либо true либо false
+//         return ['age', 'job'].includes(prop)
+//     },
+//     deleteProperty(target, prop) { // Удаление поля в объекте Proxy
+//         console.log('Deleting...', prop);
+//         delete target[prop];
+//         return true;
+//     }
+// });
 
 // Proxy functions
 
-const log = text => `Log: ${text}`;
-const fp = new Proxy(log, {
-    apply(target, thisArg, args) {
-        console.log('Calling fn...');
-        return target.apply(thisArg, args).toUpperCase();
+// const log = text => `Log: ${text}`;
+// const fp = new Proxy(log, {
+//     apply(target, thisArg, args) {
+//         console.log('Calling fn...');
+//         return target.apply(thisArg, args).toUpperCase();
+//
+//     }
+// });
 
-    }
-});
+// Proxy Classes
 
-// Classes
+// class Person {
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+// }
+//
+// const PersonProxy = new Proxy(Person, {
+//     construct(target, args) {
+//         console.log('Construct...');
+//         return new Proxy(new target(...args), {
+//             get(t, prop) {
+//                 console.log(`Getting prop ${prop}`);
+//                 return t[prop];
+//             }
+//         });
+//     }
+// });
+//
+// const p = new PersonProxy('Maxim', 30);
 
-class Person {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
-    }
-}
+// Proxy Wrapper
 
-const PersonProxy = new Proxy(Person, {
-    construct(target, args) {
-        console.log('Construct...');
-        return new Proxy(new target(...args), {
-            get(t, prop) {
-                console.log(`Getting prop ${prop}`);
-                return t[prop];
-            }
-        });
-    }
-});
+// const withDefaulvalue = (target, defaulValue) => {
+//     return new Proxy(target, {
+//         get: (obj, prop) => (prop in obj) ? obj[prop] : defaulValue
+//     })
+// };
+// // вернёт ключ и значени ключа, если ключ неправильный - вернёт 10.
+// const position = withDefaulvalue({ x: 24, y: 42 },10);
+// console.log(position);
+//
+// // Hidden properties
+//
+// const withHiddeProps = (target, prefix = '_') => {
+//     return new Proxy(target, {
+//         has: (obj, prop) => (prop in obj) && (!prop.startsWith(prefix)),
+//         ownKeys: obj => Reflect.ownKeys(obj).filter(p => !p.startsWith(prefix)),
+//         get: (obj, prop, receiver) => (prop in receiver)
+//             ? obj[prop]
+//             : void 0, // undefined
+//     });
+// };
+// const data = withHiddeProps({
+//     name: 'Vito',
+//     age: 25,
+//     _uid: '32412123213' // это будет приватное поле в объекте из-за void 0
+// });
 
-const p = new PersonProxy('Maxim', 30);
+// Optimization
+
+// const userData = [
+//     {id: 1, name: 'Vito', job: 'Fullstack', age: 25},
+//     {id: 2, name: 'Carl', job: 'Front', age: 26},
+//     {id: 3, name: 'Den', job: 'Back', age: 27},
+//     {id: 4, name: 'Joe', job: 'DevOps', age: 28}
+// ];
+
+// const index = {};
+// userData.forEach(i => index[i.id] = i);
+
+
+// const IndexArray = new Proxy(Array, {
+//     construct(target, [args]) {
+//         const index = {};
+//         args.forEach(item => index[item.id] = item);
+//
+//         return new Proxy(new target(...args), {
+//             get(arr, prop) {
+//                 switch (prop) {
+//                     case 'push':
+//                         return item => {
+//                             index[item.id] = item;
+//                             arr[prop].call(arr, item);
+//                         };
+//                     case 'findById':
+//                         return id => index[id];
+//                     default:
+//                         return arr[prop]
+//                 }
+//             }
+//         });
+//     }
+// });
+// const users = new IndexArray([
+//     {id: 1, name: 'Vito', job: 'Fullstack', age: 25},
+//     {id: 2, name: 'Carl', job: 'Front', age: 26},
+//     {id: 3, name: 'Den', job: 'Back', age: 27},
+//     {id: 4, name: 'Joe', job: 'DevOps', age: 28}
+// ]);
 
