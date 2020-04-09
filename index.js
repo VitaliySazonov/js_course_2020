4afe244b248e395a848aa05f26dca5148de1309e
Vito <topvetal@gmail.com>
2020-04-10 02:09:02 +0300

    Fetch, XMLHttpRequest (XHR), Ajax

--- index.js
+++ index.js
@@ -783,13 +783,69 @@
 //
 // WeakSet
 
-const users = [
-  {name: 'Vito'},
-  {name: 'Alex'},
-  {name: 'Irina'},
-];
-const visits = new WeakSet();
-visits.add(users[0]).add(users[1]);
-users.splice(1, 1);
-console.log(visits.has(users[0]));
-console.log(visits.has(users[1]));
+// const users = [
+//   {name: 'Vito'},
+//   {name: 'Alex'},
+//   {name: 'Irina'},
+// ];
+// const visits = new WeakSet();
+// visits.add(users[0]).add(users[1]);
+// users.splice(1, 1);
+// console.log(visits.has(users[0]));
+// console.log(visits.has(users[1]));
+
+// Fetch, Ajax, XHR
+
+const requestURL = 'https://jsonplaceholder.typicode.com/users';
+
+
+function sendRequest (method, url, body = null) {
+  // return new Promise((resolve, reject) => {
+  //   const xhr = new XMLHttpRequest();
+  //   xhr.open(method, url);
+  //   xhr.responseType = 'json';
+  //   xhr.setRequestHeader('Content-Type', 'application/json');
+  //   xhr.onload = () => {
+  //     if (xhr.status >= 400) {
+  //       reject(xhr.status, xhr.response);
+  //     } else {
+  //       resolve(xhr.status, xhr.response);
+  //     }
+  //     console.log(xhr.response); // ответ с сервера
+  //   };
+  //   xhr.onerror = () => {
+  //     reject(xhr.response); //
+  //   };
+  //   xhr.send(JSON.stringify(body));
+  // });
+  const headers = {
+    'Content-Type': 'application/json'
+  };
+  return fetch(url, {
+    method: method,
+    body: JSON.stringify(body),
+    headers: headers,
+  }).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    return response.json().then(error => {
+      const e = new Error('Smth wrong');
+      e.data = error;
+      throw e;
+    });
+  })
+}
+//
+// sendRequest('GET', requestURL)
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
+const body = {
+  name: 'Vito',
+  job: 'Fullstack',
+  age: 25
+};
+sendRequest('POST', requestURL, body)
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
+
